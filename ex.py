7082e64596671aa0eb13b01079f9f4b4f11005ec
Touhidul Alam <alamtl@ims.uni-stuttgart.de>
2018-05-08 03:05:42 +0200

    first commit

--- ex.py
+++ ex.py
@@ -13,31 +13,29 @@ df_y = df_y.reset_index()
 x_list = []
 y_list = []
 
-df_x_prev = (-5555,-5555)
-df_y_prev = (-5555,-5555)
+df_x_prev = [-5555,-5555]
+df_y_prev = [-5555,-5555]
 for i,j in zip(range(len(df_x.index)), range(len(df_y.index))):
 	'''FOR X-AXIS'''
 	''' While Same X-axis has multiple same value, keep skipping'''
 	while df_x.x[i] == df_x_prev[0]:
 		i+=1
 		continue
+
+	'''Condition to check axis '''
+	#while len(x_list) > 0 and df_x.x[i] < df_x_prev[0] and 
 	''' For first point case, where X-axis value is higher'''
 	if df_x.x[i] > df_x_prev[0]:
 
-		df_x_prev = list(df_x_prev)
 		df_x_prev[0] = df_x.x[i]
 		df_x_prev[1] = df_x.y[i]
-		df_x_prev = tuple(df_x_prev)
 
 		x_list.append((df_x.x[i], df_x.y[i]))
 
-	''' While next X-axis point has higher Y-value than current '''
 	elif df_x.y[i] > df_x_prev[1]:
-		
-		df_x_prev = list(df_x_prev)
+		''' While next X-axis point has higher Y-value than current '''
 		df_x_prev[0] = df_x.x[i]
 		df_x_prev[1] = df_x.y[i]
-		df_x_prev = tuple(df_x_prev)
 
 		x_list.append((df_x.x[i], df_x.y[i]))
 	
@@ -47,18 +45,16 @@ for i,j in zip(range(len(df_x.index)), range(len(df_y.index))):
 		continue
 
 	if df_y.y[j] > df_y_prev[1]:
-		df_y_prev = list(df_y_prev)
+
 		df_y_prev[0] = df_y.x[j]
 		df_y_prev[1] = df_y.y[j]
-		df_y_prev = tuple(df_y_prev)
 
 		y_list.append((df_y.x[j], df_y.y[j]))
 
 	elif df_y.x[j] > df_y_prev[0]:
-		df_y_prev = list(df_y_prev)
+
 		df_y_prev[0] = df_y.x[j]
 		df_y_prev[1] = df_y.y[j]
-		df_y_prev = tuple(df_y_prev)
 
 		y_list.append((df_y.x[j], df_y.y[j]))
 
